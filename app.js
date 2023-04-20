ec3075bf9d0c4f9eddd53d2dbc9ef8fc48360f84
Evgeniy Kudra <evkudra@gmail.com>
2023-04-20 22:48:42 +0300

    refactor: add class for checkbox and edit html, css, js

--- app.js
+++ app.js
@@ -155,7 +155,7 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents = function(taskListItem,checkBoxEventHandler) {
     console.log("bind list item events");
     //select ListItems children
-    var checkBox = taskListItem.querySelector("input[type=checkbox]");
+    var checkBox = taskListItem.querySelector(".checkbox");
     var editButton = taskListItem.querySelector("button.edit");
     var deleteButton = taskListItem.querySelector("button.delete");
 
