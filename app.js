dfeae138d103d8d40491967679ae9cc8bdb14353
Evgeniy Kudra <evkudra@gmail.com>
2023-04-24 20:29:51 +0300

    refactor: clean unnecessaty comments

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-//Document is the DOM can be accessed in the console with document.window.
-// Tree is from the top, html, body, p etc.
-
-//Problem: User interaction does not provide the correct results.
-//Solution: Add interactivity so the user can manage daily tasks.
-//Break things down into smaller steps and take each step at a time.
-
-
-// Event handling, user interaction is what starts the code execution.
-
 import { createNewTaskElement } from "./components/createNewTaskElement.js";
 import { deleteTask } from "./components/deleteTask.js";
 import { editTask } from "./components/editTask.js";
@@ -33,7 +23,7 @@ const addTask = function() {
   bindTaskEvents(listItem, taskCompleted);
 
   taskInput.value = "";
-}
+};
 
 
 //Mark task completed
@@ -65,7 +55,6 @@ addButton.addEventListener("click",ajaxRequest);
 
 
 //cycle over incompleteTaskHolder ul list items
-
 for (let i = 0; i < incompleteTaskHolder.children.length; i++){
   bindTaskEvents(incompleteTaskHolder.children[i],taskCompleted);
 }
@@ -75,10 +64,3 @@ for (let i = 0; i < incompleteTaskHolder.children.length; i++){
 for (let i = 0; i < completedTasksHolder.children.length; i++){
   bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
 }
-
-
-// Issues with usability don't get seen until they are in front of a human tester.
-
-//prevent creation of empty tasks.
-
-//Change edit to save when you are in edit mode.
